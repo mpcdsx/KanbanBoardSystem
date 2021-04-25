afc31824055dae79a41e6bc200f35929d582497e
mpcdsx <81625239+mpcdsx@users.noreply.github.com>
2021-04-25 13:07:52 +0800

    Update Program.cs

--- KanbanBoardSystem/Program.cs
+++ KanbanBoardSystem/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace KanbanBoardSystem
 {
@@ -6,7 +6,116 @@ namespace KanbanBoardSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userName, userDay, optionSelected;
+
+            Console.WriteLine("Training Sessions: \n");
+            Console.WriteLine("Monday: \n");
+            Console.WriteLine("Wednesday: \n");
+            Console.WriteLine("Friday: \n");
+            Console.WriteLine("Completed Session: ");
+
+            Console.WriteLine("Input your training session:");
+            Console.WriteLine("Enter your name:");
+            userName = Console.ReadLine();
+            Console.WriteLine("Enter \'m\' for Monday, \'w\' for Wednesday and \'f\' for Friday: ");
+            userDay = optionSelected = Console.ReadLine();
+
+            {
+                if (optionSelected.Equals("m"))
+                {
+                    Console.WriteLine("Your session is Monday. \n");
+                }
+                else if (optionSelected.Equals("w"))
+                {
+                    Console.WriteLine("Your session is Wednesday. \n");
+                }
+                else if (optionSelected.Equals("f"))
+                {
+                    Console.WriteLine("Your session is Friday. \n");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid selection.");
+
+                }
+
+                Console.WriteLine("Training Sessions:");
+                if (userDay.Equals("m"))
+                {
+                    Console.WriteLine("Monday");
+                }
+                else if (userDay.Equals("w"))
+                {
+                    Console.WriteLine("Wednesday");
+                }
+                else if (userDay.Equals("f"))
+                {
+                    Console.WriteLine("Friday");
+                }
+
+
+                {
+                    Console.WriteLine("Trainer Name: " + userName);
+                    Console.WriteLine();
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+
+            {
+                Console.WriteLine("Input your training session:");
+                Console.WriteLine("Enter your name:");
+                userName = Console.ReadLine();
+                Console.WriteLine("Enter \'m\' for Monday, \'w\' for Wednesday and \'f\' for Friday: ");
+                userDay = optionSelected = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+
+                if (optionSelected.Equals("m"))
+                {
+                    Console.WriteLine("Your session is Monday. \n");
+                }
+                else if (optionSelected.Equals("w"))
+                {
+                    Console.WriteLine("Your session is Wednesday. \n");
+                }
+                else if (optionSelected.Equals("f"))
+                {
+                    Console.WriteLine("Your session is Friday. \n");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid selection.");
+
+                }
+
+                Console.WriteLine("Training Sessions:");
+                if (userDay.Equals("m"))
+                {
+                    Console.WriteLine("Monday");
+                }
+                else if (userDay.Equals("w"))
+                {
+                    Console.WriteLine("Wednesday");
+                }
+                else if (userDay.Equals("f"))
+                {
+                    Console.WriteLine("Friday");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid selection");
+                }
+
+
+                {
+                    Console.WriteLine("Trainer Name: " + userName);
+
+                }
+
+            }
         }
     }
 }
