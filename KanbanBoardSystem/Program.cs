01d631c9440ecf0fdddc5911b73e759d248b70d1
mpcdsx <81625239+mpcdsx@users.noreply.github.com>
2021-05-02 22:58:10 +0800

    Update Program.cs

--- KanbanBoardSystem/Program.cs
+++ KanbanBoardSystem/Program.cs
@@ -6,116 +6,32 @@ namespace KanbanBoardSystem
     {
         static void Main(string[] args)
         {
-            string userName, userDay, optionSelected;
+            string task, requested, doing, done, optionSelected;
 
-            Console.WriteLine("Training Sessions: \n");
-            Console.WriteLine("Monday: \n");
-            Console.WriteLine("Wednesday: \n");
-            Console.WriteLine("Friday: \n");
-            Console.WriteLine("Completed Session: ");
-
-            Console.WriteLine("Input your training session:");
-            Console.WriteLine("Enter your name:");
-            userName = Console.ReadLine();
-            Console.WriteLine("Enter \'m\' for Monday, \'w\' for Wednesday and \'f\' for Friday: ");
-            userDay = optionSelected = Console.ReadLine();
-
+            Console.WriteLine("To Do List: ");
+            Console.WriteLine("Enter your task: ");
+            requested = Console.ReadLine();
+            Console.WriteLine("Click \'a\' if your task is WIP.");
+            Console.WriteLine("Click \'b\' if your task is Done.");
+            task = optionSelected = Console.ReadLine();
             {
-                if (optionSelected.Equals("m"))
-                {
-                    Console.WriteLine("Your session is Monday. \n");
-                }
-                else if (optionSelected.Equals("w"))
-                {
-                    Console.WriteLine("Your session is Wednesday. \n");
-                }
-                else if (optionSelected.Equals("f"))
-                {
-                    Console.WriteLine("Your session is Friday. \n");
-                }
+
+                if (task.Equals("a"))
+                    {
+                        Console.WriteLine("Your task is now working in progress!");
+                    }
+                else if (task.Equals("b"))
+                    {
+                    Console.WriteLine("Your task is finally done!");
+                    }
                 else
-                {
-                    Console.WriteLine("Invalid selection.");
+                    {
+                        Console.WriteLine("Invalid selection.");
 
-                }
-
-                Console.WriteLine("Training Sessions:");
-                if (userDay.Equals("m"))
-                {
-                    Console.WriteLine("Monday");
-                }
-                else if (userDay.Equals("w"))
-                {
-                    Console.WriteLine("Wednesday");
-                }
-                else if (userDay.Equals("f"))
-                {
-                    Console.WriteLine("Friday");
-                }
-
-
-                {
-                    Console.WriteLine("Trainer Name: " + userName);
-                    Console.WriteLine();
+                    }
                 }
             }
 
-            for (int i = 0; i < 2; i++)
-
-            {
-                Console.WriteLine("Input your training session:");
-                Console.WriteLine("Enter your name:");
-                userName = Console.ReadLine();
-                Console.WriteLine("Enter \'m\' for Monday, \'w\' for Wednesday and \'f\' for Friday: ");
-                userDay = optionSelected = Console.ReadLine();
-            }
-
-            for (int i = 0; i < 2; i++)
-            {
-
-                if (optionSelected.Equals("m"))
-                {
-                    Console.WriteLine("Your session is Monday. \n");
-                }
-                else if (optionSelected.Equals("w"))
-                {
-                    Console.WriteLine("Your session is Wednesday. \n");
-                }
-                else if (optionSelected.Equals("f"))
-                {
-                    Console.WriteLine("Your session is Friday. \n");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid selection.");
-
-                }
-
-                Console.WriteLine("Training Sessions:");
-                if (userDay.Equals("m"))
-                {
-                    Console.WriteLine("Monday");
-                }
-                else if (userDay.Equals("w"))
-                {
-                    Console.WriteLine("Wednesday");
-                }
-                else if (userDay.Equals("f"))
-                {
-                    Console.WriteLine("Friday");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid selection");
-                }
-
-
-                {
-                    Console.WriteLine("Trainer Name: " + userName);
-
-                }
-
-            }
         }
+
     }
-}
