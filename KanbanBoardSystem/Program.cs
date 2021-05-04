3b35bd671a74ae0941fcf338ef7eaef7880c9222
mpcdsx <mpcdsalazar@iskolarngbayan.pup.edu.ph>
2021-05-04 11:47:41 +0800

    updated

--- KanbanBoardSystem/Program.cs
+++ KanbanBoardSystem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace KanbanBoardSystem
 {
@@ -6,7 +7,47 @@ namespace KanbanBoardSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string task, requested, doing, done, optionSelected;
+
+            Console.WriteLine("To Do List: ");
+
+            List<string> toDoList = new List<string>();
+
+            toDoList.Add("Activity 1");
+            toDoList.Add("Activity 2");
+            toDoList.Add("Assignment 1");
+            toDoList.Add("Assignment 2");
+
+            for (int i = 0; i < toDoList.Count; i++)
+            {
+                Console.WriteLine(toDoList[i]);
+
+            }
+
+            Console.WriteLine("Enter your task: ");
+            requested = Console.ReadLine();
+            Console.WriteLine("Click \'a\' if your task is WIP.");
+            Console.WriteLine("Click \'b\' if your task is Done.");
+            task = optionSelected = Console.ReadLine();
+            {
+
+                if (task.Equals("a"))
+                    {
+                        Console.WriteLine("Your task is now working in progress!");
+                    }
+                else if (task.Equals("b"))
+                    {
+                    Console.WriteLine("Your task is finally done!");
+                    }
+                else
+                    {
+                        Console.WriteLine("Invalid selection.");
+
+                    }
+                }
+            }
+               
+
         }
+
     }
-}
